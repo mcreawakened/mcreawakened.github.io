1edb9cf49175ae1bbd451cf7ccfe9c9884327404
Mum <legonexoknights123@gmail.com>
2025-03-15 17:50:26 +0000

    added log out screen

--- js/cookies.js
+++ js/cookies.js
@@ -20,3 +20,12 @@ function getCookie(cname) {
     }
     return "";
 }
+
+function removeCookie(cname) {
+    if( get_cookie( name ) ) {
+        document.cookie = name + "=" +
+            ((path) ? ";path="+path:"")+
+            ((domain)?";domain="+domain:"") +
+            ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
+    }
+}
