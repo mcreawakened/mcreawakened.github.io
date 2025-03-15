e4ee19e594f81a9e4b18e9c7eca4fcb41ca2e093
Mum <legonexoknights123@gmail.com>
2025-03-15 18:47:11 +0000

    fix attempt

--- js/show_user.js
+++ js/show_user.js
@@ -64,9 +64,8 @@ function getCookie(cname) {
 
 function removeCookie(cname) {
     if( getCookie( name )!=="" ) {
-        document.cookie = name + "=" +
-            ((path) ? ";path="+path:"")+
-            ((domain)?";domain="+domain:"") +
-            ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
+
+        let expires = "expires=Thu, 01 Jan 1970 00:00:01 GMT";
+        document.cookie = name + "=" + "" + ";"+expires + ";path=/";
     }
 }
