3b08946c6501ee4d9e21727976b35287e97324ee
SF-24 <legonexoknights123@gmail.com>
2025-08-31 12:31:05 +0100

    New map

--- js/map.js
+++ js/map.js
@@ -28,11 +28,27 @@ function load() {
             document.getElementById('sharedNav').innerHTML = html
         });
 
-    fetch('https://mcreawakened.github.io/map/survival/')
+    fetch('https://mcreawakened.github.io/map/survival/index.html')
         .then((response) => {
             return response.text();
         })
         .then((html) => {
-            document.getElementById('mapDov').innerHTML = html
+            document.getElementById('mapDiv').innerHTML = html
         });
+
+    function getCookie(cname) {
+        let name = cname + "=";
+        let ca = document.cookie.split(';');
+        for(let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
 }
