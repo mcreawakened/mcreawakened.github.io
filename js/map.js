733831d5d385b16cdcb34a95003dcf20690c238b
SF-24 <legonexoknights123@gmail.com>
2025-08-31 12:41:21 +0100

    Updated map again

--- js/map.js
+++ js/map.js
@@ -28,14 +28,6 @@ function load() {
             document.getElementById('sharedNav').innerHTML = html
         });
 
-    fetch('https://mcreawakened.github.io/map/survival/index.html')
-        .then((response) => {
-            return response.text();
-        })
-        .then((html) => {
-            document.getElementById('mapDiv').innerHTML = html
-        });
-
     function getCookie(cname) {
         let name = cname + "=";
         let ca = document.cookie.split(';');
